233a65516ca1d219919a5b8444d21e8a8b9aa1d7
Gundul <sampah@mailbox.org>
2022-03-11 18:40:34 +0100

    goon

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 10:24:46 by graja             #+#    #+#             */
-/*   Updated: 2022/03/11 15:47:20 by graja            ###   ########.fr       */
+/*   Updated: 2022/03/11 18:38:05 by graja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ class Array
 
 	public:
 		Array(void): _member(0), _array(NULL){};
-		Array(unsigned int n): _member(n), _array(new T[n]) {};
-		Array(T & cpy);
-		~Array(void);
-		T &	operator=(T const & right);
+		Array(unsigned int n): _member(n), _array(new T[n]()) {};
+		Array(T & cpy): _member(cpy.size()), _array(new T[cpy.size()]()) {};
+		~Array(void){if (this->_array) delete [] this->_array;};
+//		T &	operator=(T const & right);
 
-		unsigned int	size(void);
+		unsigned int	size(void){return (this->_member);};
 };
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 10:35:45 by graja             #+#    #+#             */
-/*   Updated: 2022/03/11 15:35:01 by graja            ###   ########.fr       */
+/*   Updated: 2022/03/11 18:18:29 by graja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ int	main(void)
 {
 	int	*arr = new int[10];
 	int		i = 0;
+	Array<int>	test(10);
 
 	while (i < 10)
 	{
 		std::cout << i << ") " << arr[i] << std::endl;
 		i++;
 	}
+	delete [] arr;
 	return (0);
 }
